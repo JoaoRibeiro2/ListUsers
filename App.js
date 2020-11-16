3edc8b4b4d50e43e5b102d495d0ee4f7af6be6d1
JoaoRibeiro2 <joaoribeyro1@gmail.com>
2020-11-16 19:24:59 -0300

    rotas do sistema criadas

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import {View, Text} from 'react-native';
+import { Button, Icon } from 'react-native-elements';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import UserForm from './src/views/UserForm';
+import UserList from './src/views/UserList';
+import TelaLogin from './src/views/TelaLogin';
+
+import { UsersProvider } from './src/context/UsersContext';
+
+const Stack = createStackNavigator();
+export default App => {
+
+    return(
+    <UsersProvider>
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName={"TelaLogin"}
+                screenOptions={screenOptions}
+                >
+                <Stack.Screen 
+                name="UserList"
+                component={UserList}
+                options = {({navigation}) => {
+                    return {
+                        title: 'Lista de usuários',
+                        headerRight: () => (
+                            <Button 
+                            onPress = {() => navigation.navigate("UserForm")}
+                            type="clear"
+                            icon={<Icon name="add" size={25} color="#fff" />}
+                            
+                            />
+                            )
+                        }
+                }}
+                />
+
+                <Stack.Screen
+                name="UserForm" 
+                component={UserForm}
+                options = {{
+                    title: 'Formulario de usuários'
+                }}
+                />
+
+                <Stack.Screen
+                    name="TelaLogin"
+                    component={TelaLogin}
+                    options ={{
+                        title: "Login"
+                    }}
+                />
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    </UsersProvider>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const screenOptions = {
+    headerStyle: {
+        backgroundColor: '#f4511e'
+    },
+    headerTintColor:  '#fff',
+    headerTitleStyle: {
+        fontWeight: 'bold'
+    }
+}
+
